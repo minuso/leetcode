da331ee205c4046b7557a9e46f14816b9ed7aed9
minuso <minuscholar@gmail.com>
2019-04-12 10:08:49 +0800

    base 7: py3

---
+++ 0504/convertToBase7.py
@@ -0,0 +1,8 @@
+def convertToBase7(self, num):
+    if num == 0: return '0'
+        
+    res, n = '', abs(num)
+    while n:
+        res = str(n%7) + res
+        n //= 7
+    return res if num >= 0 else '-' + res
