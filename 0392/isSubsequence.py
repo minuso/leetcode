805490607cb74847736cfedcc0a32ae2a6cf7828
minuso <minuscholar@gmail.com>
2019-05-07 02:29:40 +0800

    is subsequence: py3

---
+++ 0392/isSubsequence.py
@@ -0,0 +1,7 @@
+def isSubsequence(self, s: str, t: str) -> bool:
+    i = -1
+    for c in s:
+        i = t.find(c, i+1) # find c in t begin at i+1
+        if i == -1:
+            return False
+    return True
