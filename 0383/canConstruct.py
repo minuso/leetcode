4bcd08f8b8191b4e81c159bce632668531d0e1a9
minuso <minuscholar@gmail.com>
2019-06-03 01:36:14 +0800

    ransom note: py3

---
+++ 0383/canConstruct.py
@@ -0,0 +1,5 @@
+def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+    for c in set(ransomNote):
+        if ransomNote.count(c) > magazine.count(c):
+            return False
+    return True
